7354268d2ee5fe01c223b49dcda110effb4af44f
charleskelvin <charles.kelvin1995@gmail.com>
2022-10-24 11:27:18 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,9 +1,9 @@
 secrets = {
-    "ssid": "Karunalayam_5g", #Router's (AP) name
-    "password": "nithiesther420", #Router's (AP) password
+    "ssid": "wifiName", #Router's (AP) name
+    "password": "password", #Router's (AP) password
 #    "timezone": +5,  # this is offset from UTC
 #    "github_token": "abcdefghij0123456789",
     "broker": "io.adafruit.com",
-    "aio_username": "charleskelvin",   ### Wirte Username here ###
-    "aio_key": "aio_YioD62DpWqusvLjrERlxmOc6PcEc",  ### Write Active Key here ###
+    "aio_username": "yourUserName",   ### Wirte Username here ###
+    "aio_key": "yourKey",  ### Write Active Key here ###
     }
